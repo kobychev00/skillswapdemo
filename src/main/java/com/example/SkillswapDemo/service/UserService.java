72c63ac53d81dc144a63d3ad1e11dadff61c6f23
air <kobychev00@bk.ru>
2025-03-05 23:10:19 +0300

    Prepared application for lesson "exceptions"

---
+++ src/main/java/com/example/SkillswapDemo/service/UserService.java
@@ -0,0 +1,5 @@
+package com.example.SkillswapDemo.service;
+
+public interface UserService {
+    String getUserById(String id);
+}
---
+++ src/main/java/com/example/SkillswapDemo/service/UserServiceImpl.java
@@ -0,0 +1,8 @@
+package com.example.SkillswapDemo.service;
+
+public class UserServiceImpl implements UserService{
+    @Override
+    public String getUserById(String id) {
+        return null;
+    }
+}
